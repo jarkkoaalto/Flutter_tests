2715a3ba64f47adca1e72b7e295d8ee584289ec2
Jarkko Aalto <jarkko.aalto@live.fi>
2019-07-26 12:08:07 +0300

    Piano app

---
+++ flutter_piano_app/lib/main.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show debugDefaultTargetPlatformOverride;
+import 'package:flutter/services.dart';
+
+ 
+import 'package:flutter_midi/flutter_midi.dart';
